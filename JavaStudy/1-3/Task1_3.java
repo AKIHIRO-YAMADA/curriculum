5dd616aee203b94eb4f34469b4fe06f0e587a434
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-01-16 04:18:35 +0900

    Task1_3
    complete

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,19 @@
+public class Task1_3 {
+    public static void main(String[] args) {
+    int plus = 10+5;
+    System.out.println(plus);
+    int minus = 20-7;
+    System.out.println(minus);
+    //10と２をかける
+    int multiply = 10 * 2;
+    System.out.println(multiply);
+    int remi = 20 % 6;
+    System.out.println(remi);
+    //string型のtownという変数を設定し有楽町を初期値として設定した
+    //string型のlineという変数を設定し線を初期値として設定した
+    String town = "有楽町";
+    String line = "線";
+    String train = town + line;
+    System.out.println(train);
+}
+}
