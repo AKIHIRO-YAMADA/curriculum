53ee2f3c491e3ffdd251650362c2be6e8a266658
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-01-15 23:51:05 +0900

    Task1_2.java
    task complete

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,11 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+        System.out.println(strArray.length);
+        System.out.println(strArray[1]);
+        int[] intArray = { 10, 20, 30, 40, 50 };
+        //5つの要素を配列している
+        System.out.println(intArray[1] + intArray[4]);
+        //2つ目の要素と5つ目の要素を足している
+}
+}
