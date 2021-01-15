6cb66965f94a55aa3e432e90ca68457c1883a6c1
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-01-15 21:40:13 +0900

    create Task1_1.java

---
+++ JavaStudy/1-0/Task1_1.java
@@ -0,0 +1,11 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+        int hogeInt = 100;
+        System.out.println(hogeInt);
+        String hogeString = "合格";
+        //小数点以下を表示
+        System.out.println(hogeString);
+        double hogeDouble = 0.2;   
+        System.out.println(hogeDouble);     
+}
+}
