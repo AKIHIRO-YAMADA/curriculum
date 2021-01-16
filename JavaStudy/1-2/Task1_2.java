d711eaf0e98158fb88e79f5468f8d7582487407c
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-01-16 20:04:00 +0900

    Task1_2
    修正

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -1,7 +1,8 @@
 public class Task1_2 {
     public static void main(String[] args) {
+        String[] country = {"JAPAN", "AMERICA", "KOREA"};
+        System.out.println(country.length);
         String[] strArray = {"りんご", "もも", "ぶどう"};
-        System.out.println(strArray.length);
         System.out.println(strArray[1]);
         int[] intArray = { 10, 20, 30, 40, 50 };
         //5つの要素を配列している
