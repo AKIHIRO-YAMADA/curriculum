eaf536bff96b7f6014b783db4b1b27bde1cc0ccf
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-01-16 19:43:29 +0900

    Task1_1
    内容修正。

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -1,10 +1,10 @@
 public class Task1_1 {
     public static void main(String[] args) {
-        int hogeInt = 100;
-        System.out.println(hogeInt);
-        String hogeString = "合格";
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        System.out.println(100);
         //小数点以下を表示
-        System.out.println(hogeString);
+        System.out.println("合格");
         double hogeDouble = 0.2;   
         System.out.println(hogeDouble);     
 }
