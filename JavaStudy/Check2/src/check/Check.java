969510b20c79a274dff377b7c86b4d0b6166cc40
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-02-26 00:54:42 +0900

    check2
    finish

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,18 +1,21 @@
 package check;
+import constants.Constants;
 
 public class Check {
+	//①【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
+	//firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
+	//なお、変数のアクセス修飾子は「private」とする。
+	 private String firstName = "山田";
+	 private String lastName = "晃大";
+
+	 public void test() {
+		 System.out.println("printNameメソッド　→　" + printName(firstName, lastName));
+	 }
 
 	public static void main(String[] args) {
-		//①【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
-		//firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
-		//なお、変数のアクセス修飾子は「private」とする。
-		private String firstName = "山田";
-		 private String lastName = "晃大";
-		 System.out.println("printNameメソッド　→　" + printName(firstName, lastName));
-	     //③【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
-	     Pet pet = new Pet("java吉","hoge");
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		 pet.introduce();
-		 RobotPet robotPet = new RobotPet("R2D2","ルーク");
+		 RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 		 robotPet.introduce();
 	}
 		//②Check.java】にてfirstNameとlastNameを引数で受け取って、
@@ -20,8 +23,5 @@ public class Check {
 		//作成した関数のアクセス修飾子は「private」とする。
 		 private static String printName(String firstName, String lastName) {
 				return firstName + lastName;
-			}
-
-
-
+		 }
 }
