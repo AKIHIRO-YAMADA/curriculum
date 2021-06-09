0ac76aa9be058e88bc09c5d6f0762bd230ae9aa3
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-06-09 15:51:48 +0900

    SE　第１章チェックテスト
    finish

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -5,22 +5,17 @@ public class Check {
 	//①【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
 	//firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
 	//なお、変数のアクセス修飾子は「private」とする。
-	 private String firstName = "山田";
-	 private String lastName = "晃大";
-	 private void printName() {
-		 System.out.println("printNameメソッド　→　" + printName(firstName, lastName));
-	 }
+	 private static String firstName = "山田";
+	 private static String lastName = "晃大";
+
 	//②Check.java】にてfirstNameとlastNameを引数で受け取って、
 			//連結して表示させるメソッド「printName」を作成しなさい。
 			//作成した関数のアクセス修飾子は「private」とする。
-	 private static String printName(String firstName, String lastName) {
-			return firstName + lastName;
+	 private static void printName(String firstName,String lastName) {
+		 System.out.println("printNameメソッド　→　" + firstName + lastName);
 	 }
-
 	public static void main(String[] args) {
-
-		Check a = new Check();
-		a.printName();
+		Check.printName(firstName,lastName);
 	//③【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		 pet.introduce();
